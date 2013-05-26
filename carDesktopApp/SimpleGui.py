5f8a1b65755e73fd2fe34965d839242e49d2ea72
Steven Cahail <scahail@yahoo.com>
2013-05-25 19:28:50 -0500

    Fixed mistake in last commit

--- carDesktopApp/SimpleGui.py
+++ carDesktopApp/SimpleGui.py
@@ -12,8 +12,8 @@ import sys
 import socket
 import tkMessageBox
 import numpy as np
-#import matplotlib.pyplot as plt
-#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 
 from random import randint
 
