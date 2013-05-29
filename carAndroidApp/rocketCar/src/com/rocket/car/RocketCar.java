a50a21b78a4a927cc8e3fa3777ad47e9666c0202
Kody Mallory <kodymallory@gmail.com>
2013-05-29 14:14:52 -0600

    Removing AChartEngine

--- carAndroidApp/rocketCar/src/com/rocket/car/RocketCar.java
+++ carAndroidApp/rocketCar/src/com/rocket/car/RocketCar.java
@@ -11,15 +11,6 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.Calendar;
 
-import org.achartengine.ChartFactory;
-import org.achartengine.GraphicalView;
-import org.achartengine.chart.PointStyle;
-import org.achartengine.model.SeriesSelection;
-import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
-import org.achartengine.renderer.XYSeriesRenderer;
-
 import android.app.Activity;
 import android.app.Service;
 import android.content.Context;
@@ -48,20 +39,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class RocketCar extends Activity implements SensorEventListener, LocationListener{
-	/** The main dataset that includes all the series that go into a chart. */
-	  private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
-	  /** The main renderer that includes all the renderers customizing a chart. */
-	  private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
-	  /** The most recently added series. */
-	  private XYSeries mCurrentSeries;
-	  /** The most recently created renderer, customizing the current series. */
-	  private XYSeriesRenderer mCurrentRenderer;
+	
+	public static final boolean Chartflag = false;
+	
 	  /** Button for creating a new series of data. */
 	  private Button mNewSeries;
 	  
 	  private TextView mSocketOut;
 	  /** The chart view that displays the data. */
-	  private GraphicalView mChartView;
+	  //private GraphicalView mChartView;
 	  
 	  private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };
 
@@ -99,22 +85,11 @@ public class RocketCar extends Activity implements SensorEventListener, Location
 	  @Override
 	  protected void onSaveInstanceState(Bundle outState) {
 	    super.onSaveInstanceState(outState);
-	    // save the current data, for instance when changing screen orientation
-	    outState.putSerializable("dataset", mDataset);
-	    outState.putSerializable("renderer", mRenderer);
-	    outState.putSerializable("current_series", mCurrentSeries);
-	    outState.putSerializable("current_renderer", mCurrentRenderer);
 	  }
 
 	  @Override
 	  protected void onRestoreInstanceState(Bundle savedState) {
 	    super.onRestoreInstanceState(savedState);
-	    // restore the current data, for instance when changing the screen
-	    // orientation
-	    mDataset = (XYMultipleSeriesDataset) savedState.getSerializable("dataset");
-	    mRenderer = (XYMultipleSeriesRenderer) savedState.getSerializable("renderer");
-	    mCurrentSeries = (XYSeries) savedState.getSerializable("current_series");
-	    mCurrentRenderer = (XYSeriesRenderer) savedState.getSerializable("current_renderer");
 	  }
 	  
 	  protected void onCreate(Bundle savedInstanceState) {
@@ -155,17 +130,6 @@ public class RocketCar extends Activity implements SensorEventListener, Location
 	    mYG = (TextView) findViewById(R.id.yG);
 	    mZG = (TextView) findViewById(R.id.zG);
 	    
-	    // set some properties on the main renderer
-	    mRenderer.setApplyBackgroundColor(true);
-	    mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
-	    mRenderer.setAxisTitleTextSize(16);
-	    mRenderer.setChartTitleTextSize(20);
-	    mRenderer.setLabelsTextSize(15);
-	    mRenderer.setLegendTextSize(15);
-	    mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
-	    mRenderer.setZoomButtonsVisible(true);
-	    mRenderer.setPointSize(5);
-        
 	    // the button that handles the new series of data creation
 	    mNewSeries = (Button) findViewById(R.id.new_series);
 	    mNewSeries.setOnClickListener(new View.OnClickListener() {
@@ -179,62 +143,8 @@ public class RocketCar extends Activity implements SensorEventListener, Location
 	    super.onResume();
 	    //Sensor Management
 	    mSensorManager.registerListener(this, mAccelerometer,SensorManager.SENSOR_DELAY_GAME);
-	    if (mChartView == null) {
-	      LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
-	      mChartView = ChartFactory.getLineChartView(this, mDataset, mRenderer);
-	      // enable the chart click events
-	      mRenderer.setClickEnabled(true);
-	      mRenderer.setSelectableBuffer(10);
-	      
-	      for(int i=0;i<3;i++){
-	      //Add Initial Series
-	      String seriesTitle = "Series " + (mDataset.getSeriesCount() + 1);
-	        // create a new series of data
-	        XYSeries series = new XYSeries(seriesTitle);
-	        mDataset.addSeries(series);
-	        mCurrentSeries = series;
-	        // create a new renderer for the new series
-	        XYSeriesRenderer renderer = new XYSeriesRenderer();
-	        renderer.setColor(COLORS[i]);
-	        mRenderer.addSeriesRenderer(renderer);
-	        // set some renderer properties
-	        renderer.setPointStyle(PointStyle.CIRCLE);
-	        renderer.setFillPoints(true);
-	        renderer.setDisplayChartValues(true);
-	        renderer.setDisplayChartValuesDistance(10);
-	        mCurrentRenderer = renderer;
-	        setSeriesWidgetsEnabled(true);
-	        mChartView.repaint();
-	      }
 	      //Initializing File
 	      //writeData("x,y,z\r\n",filename);
-
-	      //Adding Chart Click Listener
-	      mChartView.setOnClickListener(new View.OnClickListener() {
-	        public void onClick(View v) {
-	          // handle the click event on the chart
-	          SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
-	          if (seriesSelection == null) {
-	            Toast.makeText(RocketCar.this, "No chart element", Toast.LENGTH_SHORT).show();
-	          } else {
-	            // display information of the clicked point
-	            Toast.makeText(
-	            		RocketCar.this,
-	                "Chart element in series index " + seriesSelection.getSeriesIndex()
-	                    + " data point index " + seriesSelection.getPointIndex() + " was clicked"
-	                    + " closest point value X=" + seriesSelection.getXValue() + ", Y="
-	                    + seriesSelection.getValue(), Toast.LENGTH_SHORT).show();
-	          }
-	        }
-	      
-	      });
-	      layout.addView(mChartView, new LayoutParams(LayoutParams.FILL_PARENT,
-	          LayoutParams.FILL_PARENT));
-	      boolean enabled = mDataset.getSeriesCount() > 0;
-	      setSeriesWidgetsEnabled(enabled);
-	    } else {
-	      mChartView.repaint();
-	    }
 	  }
 	  protected void playSound(int duration,int type)
 	  {
@@ -265,40 +175,25 @@ public class RocketCar extends Activity implements SensorEventListener, Location
 		  mSensorManager.unregisterListener(this);
 	  }
 
-	  /**
-	   * Enable or disable the add data to series widgets
-	   * 
-	   * @param enabled the enabled state
-	   */
-	  private void setSeriesWidgetsEnabled(boolean enabled) {
-
-	  }
 	  public void writeData(String data,String strFilePath)
       {
-
           PrintWriter csvWriter;
           try
           {
-
               File file = new File(strFilePath);
               if(!file.exists()){
                   file = new File(strFilePath);
               }
               csvWriter = new  PrintWriter(new FileWriter(file,true));
-
-
               csvWriter.print(data);
-
-
               csvWriter.close();
-
-
           }
           catch (Exception e)
           {
               e.printStackTrace();
           }
       }
+
 	  public String getIP()
 	  {
 		  EditText mIPView = (EditText) findViewById(R.id.mIpAddr);
@@ -307,14 +202,15 @@ public class RocketCar extends Activity implements SensorEventListener, Location
 	  	  mNetTask.SetSocketIP(mIP);
 	  	  return mIP;
 	  }
+
 	public void mLog(String s)
 	{
 		writeData(s+"\r\n",filename);
 	}
 
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	public void onAccuracyChanged(Sensor sensor, int accuracy)
+	{
 		// TODO Auto-generated method stub
-		
 	}
 
 	public void onSensorChanged(SensorEvent event) {
@@ -350,8 +246,6 @@ public class RocketCar extends Activity implements SensorEventListener, Location
 				{
 					playSound(200,0);
 					mReadyToDrop = true;
-					mCurrentSeries = mDataset.getSeriesAt(2);
-			        mCurrentSeries.add(timecount, y);
 					if(mRocketFired)
 						mRocketFired = false;
 				}
@@ -375,19 +269,11 @@ public class RocketCar extends Activity implements SensorEventListener, Location
 					{
 						playSound(1000,3);
 						mRocketFired = true;
-						mCurrentSeries = mDataset.getSeriesAt(1);
-				        mCurrentSeries.add(timecount, y);
 				        mAngleCount = 0;
 				        mLevelCount = 0;
 						mReadyToDrop = false;
 					}
 				}
-				// add a new data point to the current series
-				//mCurrentSeries = mDataset.getSeriesAt(0);
-		        //mCurrentSeries.add(timecount, y);
-		      //writeData(String.valueOf(x)+","+String.valueOf(y)+","+String.valueOf(z)+"\r\n",filename);
-		        // repaint the chart such as the newly added point to be visible
-		        //mChartView.repaint();
 		        timecount++;
 			}
 		}
